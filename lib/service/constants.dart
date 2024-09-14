3a3f56b56913e3156fd9f41e8ec57b487c005314
Raaz <raazraz621@gmail.com>
2024-09-15 00:08:02 +0530

    ios build ipa

--- lib/service/constants.dart
+++ lib/service/constants.dart
@@ -1,6 +1,6 @@
 // const MONGO_URL= "mongodb+srv://johnathikalam:YxCBXa7jlVKMrB2J@cluster0.gog59al.mongodb.net/karata_limousine?retryWrites=true&w=majority&appName=Cluster0";
-// const MONGO_URL= "mongodb+srv://anulisba:aCZHjI8NyQLOHV2d@fleetmanager.mdvsoan.mongodb.net/?retryWrites=true&w=majority&appName=fleetmanager";
-const MONGO_URL= "mongodb+srv://anulisba:uOviydafsjvOA5dt@manager-fleet.bopau.mongodb.net/?retryWrites=true&w=majority&appName=manager-fleet";
+const MONGO_URL= "mongodb+srv://anulisba:aCZHjI8NyQLOHV2d@fleetmanager.mdvsoan.mongodb.net/?retryWrites=true&w=majority&appName=fleetmanager";
+// const MONGO_URL= "mongodb+srv://anulisba:uOviydafsjvOA5dt@manager-fleet.bopau.mongodb.net/?retryWrites=true&w=majority&appName=manager-fleet";
 
 const COLLECTION_DRIVERS = "drivers";
 const COLLECTION_TEMPVEHICLES = "tempVehicles";
